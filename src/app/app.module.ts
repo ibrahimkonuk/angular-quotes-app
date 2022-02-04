9784d4b181d8328fb740f5abc03b28e2be0e98cb
ikonuk <28847139+ibrahimkonuk@users.noreply.github.com>
2022-02-04 20:12:50 +0100

    Create messages component and integrate to the app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,15 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { QuotesComponent } from './components/quotes/quotes.component';
 import { QuoteDetailComponent } from './components/quote-detail/quote-detail.component';
+import { MessagesComponent } from './components/messages/messages.component';
 
 @NgModule({
-  declarations: [AppComponent, QuotesComponent, QuoteDetailComponent],
+  declarations: [
+    AppComponent,
+    QuotesComponent,
+    QuoteDetailComponent,
+    MessagesComponent,
+  ],
   imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
