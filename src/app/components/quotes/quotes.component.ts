db2d014dfb50ee0e9ce33a538349dbe52ee17854
ikonuk <28847139+ibrahimkonuk@users.noreply.github.com>
2022-02-04 15:06:51 +0100

    List quotes in the component template

--- src/app/components/quotes/quotes.component.ts
+++ src/app/components/quotes/quotes.component.ts
@@ -1,11 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { Quote } from 'src/app/interfaces/quote';
+import { QUOTES } from 'src/app/mock-data/quotes';
 
 @Component({
   selector: 'app-quotes',
   templateUrl: './quotes.component.html',
   styleUrls: ['./quotes.component.scss'],
 })
-export class QuotesComponent implements OnInit {
+export class QuotesComponent implements OnInit{
+
+  quotes = QUOTES;
+  }
+
   constructor() {}
 
   ngOnInit(): void {}
