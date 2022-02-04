d5925cb91aef5ee323a301053b8e4e3da9d183a5
ikonuk <28847139+ibrahimkonuk@users.noreply.github.com>
2022-02-04 13:28:59 +0100

    Register quotes component in app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { QuotesComponent } from './components/quotes/quotes.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, QuotesComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
