b3abe51ac17ff590b139e089c873033999410aa2
ikonuk <28847139+ibrahimkonuk@users.noreply.github.com>
2022-02-04 13:30:01 +0100

    Import FormsModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { QuotesComponent } from './components/quotes/quotes.component';
@@ -11,7 +12,8 @@ import { QuotesComponent } from './components/quotes/quotes.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
