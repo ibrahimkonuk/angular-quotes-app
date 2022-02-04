852ca544429e4c2eca023c1360dfa541ec749e7f
ikonuk <28847139+ibrahimkonuk@users.noreply.github.com>
2022-02-04 13:44:49 +0100

    Create quote interface and mock data

---
+++ src/app/mock-data/quotes.ts
@@ -0,0 +1,23 @@
+import { Quote } from '../interfaces/quote';
+
+export const QUOTES: Quote[] = [
+  {
+    id: 1,
+    content:"Strive not to be a success, but rather to be of value.",
+    author:"Albert Einstein"},
+  {
+    id: 2,
+    content: "Continuous improvement is better than delayed perfection.",
+    author: "Mark Twain",
+  },
+  {
+    id: 3,
+    content: "Kindness in words creates confidence. Kindness in thinking creates profoundness. Kindness in giving creates love.",
+    author: "Lao Tzu",
+  },
+  {
+    id: 4,
+    content: "Be who you are and say what you feel, because those who mind don't matter, and those who matter don't mind.",
+    author: "Bernard M. Baruch"
+  },
+];
