c87c28854dae6a68cdb5ebbebdcf36e691a5b673
ikonuk <28847139+ibrahimkonuk@users.noreply.github.com>
2022-02-05 21:34:59 +0100

    Add RouterOutlet to the app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { QuotesComponent } from './components/quotes/quotes.component';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'quotes', component: QuotesComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
