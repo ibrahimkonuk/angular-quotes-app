95f4939d1c8415109a03990059cb7fcec9f9c319
ikonuk <28847139+ibrahimkonuk@users.noreply.github.com>
2022-02-05 22:40:13 +0100

    Add quote detail route and update related templates
    Add quote detail link and remove quote detail selector from quotes component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { QuotesComponent } from './components/quotes/quotes.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { QuoteDetailComponent } from './components/quote-detail/quote-detail.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
   { path: 'dashboard', component: DashboardComponent },
   { path: 'quotes', component: QuotesComponent },
+  { path: 'detail/:id', component: QuoteDetailComponent },
 ];
 
 @NgModule({
