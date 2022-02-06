9ce95006c5ae0e56c2da6d2ad7030d38effa2176
ikonuk <28847139+ibrahimkonuk@users.noreply.github.com>
2022-02-07 00:01:40 +0100

    Create error handler method

--- src/app/services/quote.service.ts
+++ src/app/services/quote.service.ts
@@ -4,6 +4,7 @@ import { Quote } from '../interfaces/quote';
 import { QUOTES } from '../mock-data/quotes';
 import { MessageService } from './message.service';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { catchError, map, tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
@@ -12,7 +13,10 @@ export class QuoteService {
   private quotesUrl = 'api/quotes'; // URL to web api
 
   getQuotes(): Observable<Quote[]> {
-    return this.http.get<Quote[]>(this.quotesUrl);
+    return this.http.get<Quote[]>(this.quotesUrl).pipe(
+      tap(() => this.log('fetched quotes')),
+      catchError(this.handleError<Quote[]>('getQuotes', []))
+    );
   }
 
   getQuote(id: number): Observable<Quote> {
@@ -20,11 +24,30 @@ export class QuoteService {
     this.log(`fetched quote id=${id}`);
     return of(quote);
   }
-  constructor(private messageService: MessageService) {}
 
   /** Log a QuoteService message with the MessageService */
   private log(message: string) {
     this.messageService.add(`QuoteService: ${message}`);
+  }
+
+  /**
+   * Handle Http operation that failed.
+   * Let the app continue.
+   *
+   * @param operation - name of the operation that failed
+   * @param result - optional value to return as the observable result
+   */
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
+
+      // TODO: better job of transforming error for user consumption
+      this.log(`${operation} failed: ${error.message}`);
+
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
   }
 
   constructor(
