e7869b5cd7b082a0615d31b6f8eaa57b7394f80f
ikonuk <28847139+ibrahimkonuk@users.noreply.github.com>
2022-02-07 22:00:22 +0100

    Add message service unit test cases

--- src/app/services/message.service.spec.ts
+++ src/app/services/message.service.spec.ts
@@ -13,4 +13,19 @@ describe('MessageService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should define messages', () => {
+    expect(service.messages).toBeDefined();
+  });
+
+  it('should add new message', () => {
+    service.add('Test message');
+    expect(service.messages[0]).toBe('Test message');
+  });
+
+  it('should clear messages', () => {
+    service.add('Message');
+    service.clear();
+    expect(service.messages.length).toBe(0);
+  });
 });
