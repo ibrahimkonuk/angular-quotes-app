26365368025bcc8b894032a79b64058f95b348c9
ikonuk <28847139+ibrahimkonuk@users.noreply.github.com>
2022-02-07 22:01:18 +0100

    Add dashboard component unit test cases

--- src/app/components/dashboard/dashboard.component.spec.ts
+++ src/app/components/dashboard/dashboard.component.spec.ts
@@ -1,10 +1,12 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { DashboardComponent } from './dashboard.component';
+import { Quote } from 'src/app/interfaces/quote';
 
 describe('DashboardComponent', () => {
   let component: DashboardComponent;
   let fixture: ComponentFixture<DashboardComponent>;
+  let quote: Quote;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -17,9 +19,27 @@ describe('DashboardComponent', () => {
     fixture = TestBed.createComponent(DashboardComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    quote = { id: 1, content: 'content', author: 'author' };
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should create variables', () => {
+    expect(component.quotes).toBeDefined();
+    expect(component.selectedQuote).toBeDefined;
+    expect(component.messageService).toBeDefined;
+  });
+
+  it('should render title', () => {
+    const dom = fixture.debugElement.nativeElement;
+    expect(dom.querySelector('h3').textContent).toBe('Top Quotes');
+  });
+
+  it('should set selected quote', () => {
+    component.onSelect(quote);
+    expect(component.selectedQuote?.content).toBe('content');
+    expect(component.selectedQuote?.author).toBe('author');
   });
 });
